94e005c1c56842bb1f03b23986934cb7b5229c88
JorgeCorral <jorgeqaracter4@gmail.com>
2025-10-22 15:07:52 -0300

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/', '/api/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/private/', '/admin/', '/api/'],
+      },
+    ],
     sitemap: 'https://atherosfilms.com.br/sitemap.xml',
   }
 }
